f31d090078c90e80d245646eca8accd368cadc18
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-01-13 09:57:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
   resource "null_resource" "example" {
       triggers = {
-           uuid()
+          uuid = uuid()
       }
       provisioner "local-exec" {
           command="lsb_release -a;sudo apt-get install google-cloud-sdk;"
